8dfc56c4ac582e57f71c64d29091ab072572bea9
Danilo Pereira <fddspdanilo@gmail.com>
2020-06-09 15:51:32 -0300

    Teste

--- Agenda_ES/src/br/edu/fafic/es/Pessoa.java
+++ Agenda_ES/src/br/edu/fafic/es/Pessoa.java
@@ -10,5 +10,36 @@ package br.edu.fafic.es;
  * @author danso
  */
 public class Pessoa {
-    
+    String nome;
+    String telefone;
+
+    public Pessoa(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", telefone=" + telefone + '}';
+    }
+    public void teste(){
+        System.out.println("Nao faz nada");
+    }
+            
 }
